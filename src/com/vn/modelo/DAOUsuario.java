77c43fb525482fd9cfca27bbb038878a7452b90d
JJ <t_mac055@hotmail.com>
2019-11-22 17:51:55 +0100

    miau inicializado ahora si

---
+++ src/com/vn/modelo/DAOUsuario.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.modelo;
+
+import com.vn.modelo.Usuario;
+import com.vn.modelo.interfaces.IDAOUsuario;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+public class DAOUsuario implements IDAOUsuario{
+    
+    private List<Usuario> listaUsuarios;
+    
+    public DAOUsuario() {
+        listaUsuarios = new ArrayList<>();
+    }
+    
+    @Override
+    public Usuario crear(Usuario nuevoUsuario){
+        listaUsuarios.add(nuevoUsuario);
+        return null;
+    }
+    @Override
+    public void modificar(int id, String email, String pwd, String nombre, int edad){
+        ;
+    }
+    @Override
+    public boolean eliminar(int id){
+        return true;
+    }
+    @Override
+    public Usuario leerUno(int id){
+        return null;
+    }
+    @Override
+    public Usuario leerUno(String email){
+        return null;
+    }
+    @Override
+    public List<Usuario> leerTodos(String nombre){
+        return listaUsuarios;
+    }
+    @Override
+    public List<Usuario> leerTodos(){
+        return listaUsuarios;
+    }
+    
+}
---
+++ src/com/vn/modelo/Usuario.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.modelo;
+
+/**
+ *
+ * @author PC
+ */
+public class Usuario {
+    private int id;
+    private String email;
+    private String password;
+    private String name;
+    private int age;
+
+    public Usuario(int id, String email, String password, String name, int age) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
