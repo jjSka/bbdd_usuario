5ed4328692ed09a634a3b4955f7ef4504b2be1ce
Álvaro Gómez Pérez <al0@hotmail.es>
2019-11-22 17:57:32 +0100

    Alo Rodriguez noseke

---
+++ 06_bbdd_usuario/src/com/vn/modelo/SQLConnection.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.modelo;
+
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author PC
+ */
+public class SQLConnection {
+    //jdbc:derby://localhost:1527/db_users
+    public Connection openConnection(String urlDB, String user, String pass){
+        Connection con=null;
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+            DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+            con = DriverManager.getConnection(urlDB, user, pass);
+        } catch (Exception ex) {
+            Logger.getLogger(SQLConnection.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error creating connection");
+        }
+         return con;
+    }
+    public void closeConnection(Connection con){
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(SQLConnection.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Error closing connection");
+        }
+    }
+}
