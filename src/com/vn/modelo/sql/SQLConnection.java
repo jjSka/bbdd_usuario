547af538b17d4ecb1942265f630b29462cba9d0c
Álvaro Gómez Pérez <al0@hotmail.es>
2019-11-25 09:17:29 +0100

    eeee

--- src/com/vn/modelo/sql/SQLConnection.java
+++ src/com/vn/modelo/sql/SQLConnection.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.vn.modelo;
+package com.vn.sql;
 
 import static java.lang.System.out;
 import java.sql.Connection;
