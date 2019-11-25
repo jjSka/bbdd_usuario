6fd19c5ae3aa14b2c09b0568fa851c5b5311526a
JJ <t_mac055@hotmail.com>
2019-11-25 09:58:06 +0100

    DAOUsuario metodos

--- src/com/vn/modelo/DAOUsuario.java
+++ src/com/vn/modelo/DAOUsuario.java
@@ -14,42 +14,65 @@ import java.util.List;
  *
  * @author PC
  */
-public class DAOUsuario implements IDAOUsuario{
-    
+public class DAOUsuario implements IDAOUsuario {
+
     private List<Usuario> listaUsuarios;
-    
+
     public DAOUsuario() {
         listaUsuarios = new ArrayList<>();
     }
-    
+
     @Override
-    public Usuario crear(Usuario nuevoUsuario){
+    public Usuario crear(Usuario nuevoUsuario) {
         listaUsuarios.add(nuevoUsuario);
         return null;
     }
+
     @Override
-    public void modificar(int id, String email, String pwd, String nombre, int edad){
-        ;
+    public void modificar(int id, String email, String pwd, String nombre, int edad) {
+        for (Usuario usuario : listaUsuarios) {
+            if (usuario.getId() == id) {
+                usuario.setEmail(email);
+                usuario.setPassword(pwd);
+                usuario.setName(nombre);
+                usuario.setAge(edad);
+            }
+        }
     }
+
     @Override
-    public boolean eliminar(int id){
+    public boolean eliminar(int id) {
         return true;
     }
+
     @Override
-    public Usuario leerUno(int id){
+    public Usuario leerUno(int id) {
+        for (Usuario usuario : listaUsuarios) {
+            if(usuario.getId()==id)
+                return usuario;
+        }
         return null;
     }
+
     @Override
-    public Usuario leerUno(String email){
+    public Usuario leerUno(String email) {
         return null;
     }
+
     @Override
-    public List<Usuario> leerTodos(String nombre){
+    public List<Usuario> leerTodos(String nombre) {
+        List<Usuario> listaNombreUsuarios= new ArrayList<Usuario>();
+        for (Usuario usuario : listaUsuarios) {
+            if(usuario.getName().equals(nombre)){
+                listaNombreUsuarios.add(usuario);
+            }
+        }
+        return listaNombreUsuarios;
+    }
+
+    @Override
+    public List<Usuario> leerTodos() {
         return listaUsuarios;
     }
-    @Override
-    public List<Usuario> leerTodos(){
-        return listaUsuarios;
-    }
-    
+
 }
